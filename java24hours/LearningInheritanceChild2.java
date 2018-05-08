070ba39d1d72e44a39366b920d03833a37066b85
SPANDIAR <sankar.pandiarajan@gmail.com>
2018-05-07 18:31:16 -0700

    Added Hotel

--- java24hours/LearningInheritanceChild2.java
+++ java24hours/LearningInheritanceChild2.java
@@ -5,6 +5,15 @@ import java.util.ArrayList;
 public class LearningInheritanceChild2 extends LearningInheritenceParent{
 	
 	private String city;
+	private String hotel;
+
+	public String getHotel() {
+		return hotel;
+	}
+
+	public void setHotel(String hotel) {
+		this.hotel = hotel;
+	}
 
 	public String getCity() {
 		return city;
@@ -17,16 +26,17 @@ public class LearningInheritanceChild2 extends LearningInheritenceParent{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String[] locations = {"Santa Cruz",  "Mission Peaks",  "Bayland Parks", "San Jose", "Half Moon Bay", "PG&E Trail", "FairWood Park", "Sunnyvale"};
+		String[] locations = {"Santa Cruz",  "Mission Peaks",  "Bayland Parks", "San Jose", "Marriot", "Half Moon Bay", "PG&E Trail", "FairWood Park", "Sunnyvale", "Hilton"};
 		ArrayList<LearningInheritanceChild2> Trip = new ArrayList<LearningInheritanceChild2>();
 		int j=0;
 		
-		for(int i=0;i<((locations.length)/4);i++){
+		for(int i=0;i<((locations.length)/5);i++){
 			LearningInheritanceChild2 tripObject = new LearningInheritanceChild2();
 			tripObject.setBeaches(locations[j++]);
 			tripObject.setMountains(locations[j++]);
 			tripObject.setParks(locations[j++]);
 			tripObject.setCity(locations[j++]);
+			tripObject.setHotel(locations[j++]);
 			Trip.add(tripObject);
 		}
 		
@@ -37,7 +47,7 @@ public class LearningInheritanceChild2 extends LearningInheritenceParent{
 			System.out.println("Mountains in Trip " + k + " is: " + Trip.get(k).getMountains());
 			System.out.println("Parks in Trip " + k + " is: " + Trip.get(k).getParks());
 			System.out.println("City in Trip " + k + " is: " + Trip.get(k).getCity());
-			
+			System.out.println("Hotel in Trip " + k + " is: " + Trip.get(k).getHotel());
 		}
 
 	}
